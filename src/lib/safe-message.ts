0128d5f03d28929649b343a3992f2fb464433871
lumoswiz <lumoswiz@protonmail.com>
2025-07-12 18:54:51 +0200

    refactor: decodeSafeMessage return Result and catch errors

--- src/lib/safe-message.ts
+++ src/lib/safe-message.ts
@@ -1,6 +1,6 @@
 import { satisfies } from 'semver';
-import { hashMessage, hashTypedData, isHex, fromHex, Hash } from 'viem';
-import type { EIP712TypedData, MinimalSafeInfo } from '../types';
+import { hashMessage, hashTypedData, isHex, fromHex, type Hash } from 'viem';
+import type { EIP712TypedData, MinimalSafeInfo, Result } from '../types';
 
 const EQ_OR_GT_1_3_0 = '>=1.3.0';
 
@@ -27,11 +27,8 @@ const generateSafeMessageTypedData = (
   }
 
   const domain = satisfies(version, EQ_OR_GT_1_3_0)
-    ? {
-        chainId: Number(BigInt(chainId)),
-        address,
-      }
-    : { address };
+    ? { chainId: Number(chainId), verifyingContract: address }
+    : { verifyingContract: address };
 
   return {
     domain,
@@ -61,13 +58,20 @@ const getDecodedMessage = (message: string): string => {
 export function decodeSafeMessage(
   message: string | EIP712TypedData,
   safe: MinimalSafeInfo
-): DecodedSafeMessage {
-  const decodedMessage =
-    typeof message === 'string' ? getDecodedMessage(message) : message;
+): Result<DecodedSafeMessage, string> {
+  try {
+    const decodedMessage =
+      typeof message === 'string' ? getDecodedMessage(message) : message;
 
-  return {
-    decodedMessage,
-    safeMessageMessage: generateSafeMessageMessage(decodedMessage),
-    safeMessageHash: generateSafeMessageHash(safe, decodedMessage),
-  };
+    const safeMessageMessage = generateSafeMessageMessage(decodedMessage);
+    const safeMessageHash = generateSafeMessageHash(safe, decodedMessage);
+
+    return {
+      status: 'ok',
+      value: { decodedMessage, safeMessageMessage, safeMessageHash },
+    };
+  } catch (e) {
+    const err = e instanceof Error ? e.message : String(e);
+    return { status: 'error', error: err };
+  }
 }
