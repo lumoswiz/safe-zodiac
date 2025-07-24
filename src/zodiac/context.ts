d8b767fc076be7cdd64d9eb3de16316e0fcd81e5
lumoswiz <97650124+lumoswiz@users.noreply.github.com>
2025-07-24 17:02:40 +0200

    refactor: move resolveSafeContext to context module (#43)

---
+++ src/zodiac/context.ts
@@ -0,0 +1,56 @@
+import { Address, isAddressEqual } from 'viem';
+import { ResolvedSafeContext, Result } from '../types';
+import { SafeContractSuite } from '../lib/safe';
+import { makeError, makeOk, matchResult } from '../lib/utils';
+
+export async function resolveSafeContext(
+  safeSuite: SafeContractSuite,
+  safe: Address,
+  owner: Address,
+  maybeSaltNonce: bigint | undefined
+): Promise<Result<ResolvedSafeContext>> {
+  if (maybeSaltNonce !== undefined) {
+    const predictedRes = await safeSuite.calculateSafeAddress(
+      [owner],
+      maybeSaltNonce
+    );
+    return matchResult(predictedRes, {
+      ok: ({ value: predictedAddress }) => {
+        if (!isAddressEqual(predictedAddress, safe)) {
+          return makeError(
+            `Provided Safe address (${safe}) does not match predicted CREATE2 address (${predictedAddress})`
+          );
+        }
+        return makeOk({
+          safeAddress: predictedAddress,
+          saltNonce: maybeSaltNonce,
+          deployed: false,
+        });
+      },
+      error: ({ error }) => makeError(error),
+    });
+  }
+
+  const ownersResult = await safeSuite.getOwners(safe);
+  return matchResult(ownersResult, {
+    ok: ({ value: owners }) => {
+      const [onlyOwner] = owners;
+      if (
+        !onlyOwner ||
+        owners.length !== 1 ||
+        !isAddressEqual(onlyOwner, owner)
+      ) {
+        return makeError(
+          new Error('Safe is not a valid 1/1 owner configuration.')
+        );
+      }
+
+      return makeOk({
+        safeAddress: safe,
+        saltNonce: null,
+        deployed: true,
+      });
+    },
+    error: ({ error }) => makeError(error),
+  });
+}
