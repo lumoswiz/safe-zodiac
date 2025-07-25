3f5fac510987f2678574db67fba6428137824503
lumoswiz <lumoswiz@protonmail.com>
2025-07-25 09:34:20 +0200

    fix: improve resolveSafeContext to detect deployed Safe via address

--- src/zodiac/context.ts
+++ src/zodiac/context.ts
@@ -1,7 +1,12 @@
 import { Address, isAddressEqual } from 'viem';
 import { ResolvedSafeContext, Result } from '../types';
 import { SafeSuite } from '../safe';
-import { makeError, makeOk, matchResult } from '../shared/utils';
+import {
+  isContractDeployed,
+  makeError,
+  makeOk,
+  matchResult,
+} from '../shared/utils';
 
 export async function resolveSafeContext(
   safeSuite: SafeSuite,
@@ -9,18 +14,49 @@ export async function resolveSafeContext(
   owner: Address,
   maybeSaltNonce: bigint | undefined
 ): Promise<Result<ResolvedSafeContext>> {
+  const deployed = await isContractDeployed(safeSuite.client, safe);
+
+  if (deployed) {
+    const ownersResult = await safeSuite.getOwners(safe);
+    return matchResult(ownersResult, {
+      ok: ({ value: owners }) => {
+        const [onlyOwner] = owners;
+        if (
+          !onlyOwner ||
+          owners.length !== 1 ||
+          !isAddressEqual(onlyOwner, owner)
+        ) {
+          return makeError(
+            new Error('Safe is not a valid 1/1 owner configuration.')
+          );
+        }
+
+        return makeOk({
+          safeAddress: safe,
+          saltNonce: null,
+          deployed: true,
+        });
+      },
+      error: ({ error }) => makeError(error),
+    });
+  }
+
   if (maybeSaltNonce !== undefined) {
     const predictedRes = await safeSuite.calculateSafeAddress(
       [owner],
       maybeSaltNonce
     );
+
     return matchResult(predictedRes, {
       ok: ({ value: predictedAddress }) => {
         if (!isAddressEqual(predictedAddress, safe)) {
           return makeError(
-            `Provided Safe address (${safe}) does not match predicted CREATE2 address (${predictedAddress})`
+            new Error(
+              `Provided Safe address (${safe}) does not match predicted CREATE2 address (${predictedAddress})`
+            )
           );
         }
+
         return makeOk({
           safeAddress: predictedAddress,
           saltNonce: maybeSaltNonce,
@@ -31,26 +67,7 @@ export async function resolveSafeContext(
     });
   }
 
-  const ownersResult = await safeSuite.getOwners(safe);
-  return matchResult(ownersResult, {
-    ok: ({ value: owners }) => {
-      const [onlyOwner] = owners;
-      if (
-        !onlyOwner ||
-        owners.length !== 1 ||
-        !isAddressEqual(onlyOwner, owner)
-      ) {
-        return makeError(
-          new Error('Safe is not a valid 1/1 owner configuration.')
-        );
-      }
-
-      return makeOk({
-        safeAddress: safe,
-        saltNonce: null,
-        deployed: true,
-      });
-    },
-    error: ({ error }) => makeError(error),
-  });
+  return makeError(
+    new Error('Safe is not deployed and no saltNonce was provided.')
+  );
 }
